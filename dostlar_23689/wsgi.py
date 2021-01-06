79c04d78d73d73d8990a5ef2453944507e4c386e
Crowdbotics <team@crowdbotics.com>
2021-01-06 04:05:12 +0000

    Update models

--- dostlar_23689/wsgi.py
+++ dostlar_23689/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dostlar_23689.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dostlar_23689.settings")
 
 application = get_wsgi_application()
